ef2d7ad4953eb7ddadfd2bbf0ed1110cade3e5c9
Ruikang Liu <15666613559@163.com>
2019-04-20 12:23:49 +0800

    ast ver2.1

--- DB/src/AST.h
+++ DB/src/AST.h
@@ -5,38 +5,54 @@
 
 namespace DB {
 
+	
+	
+
+
 	namespace VM{
-		enum class col_t_t { INT, CHAR, VARCHAR };
-		struct constraint_t_t { enum { PK = 1, FK = 2, NOT_NULL = 4, DEFAULT = 8, }; };
-		struct ColumnInfo {
-			std::string columnName;
-			col_t_t col_t_;
-			unsigned int str_len_;      // used when col_t_ = `CHAR` or `VARCHAR`
-			unsigned int constraint_t_;
-			std::string fkTable;
-			std::string defaultStr;
-			int defaultInt;
-		};
+		
+		using RetValue = std::variant<bool, int, std::string>;
 
 		bool is_PK(const std::string& tableName, const std::string& col_name);
         bool is_FK(const std::string& tableName, const std::string& col_name);
         bool is_not_null(const std::string& tableName, const std::string& col_name);
         bool is_default_col(const std::string& tableName, const std::string& col_name);
-        col_t_t get_default_value(const std::string& tableName, const std::string& col_name);
+		RetValue get_default_value(const std::string& tableName, const std::string& col_name);
 
 		// onlt used when creating table
-        void insert_column(const std::string&, ColumnInfo*);
-
-		using RetValue = std::variant<bool, int, std::string>;
-
+        //void insert_column(const std::string&, ColumnInfo*);
+		
 		struct Row
 		{
-			RetValue getValue(Row* row, const std::string& tableName, const std::string& columnName) { return 0; }
+
 		};
+		RetValue getValue(Row* row, const std::string& tableName, const std::string& columnName) { return 0; }
+
 	}
 
 
-	namespace DML{
+	namespace AST{
+		//===========================================================
+		//DDL
+		enum class col_t_t { INT, CHAR, VARCHAR };
+		struct ColumnInfo {
+			std::string columnName;
+			col_t_t col_t_;
+			unsigned int str_len_;      // used when col_t_ = `CHAR` or `VARCHAR`
+			unsigned int constraint_t_;		//constraint_t_t { PK = 1, FK = 2, NOT_NULL = 4, DEFAULT = 8, }
+			std::string fkTable;
+			std::string defaultStr;
+			int defaultInt;
+		};
+		struct CreateTableInfo
+		{
+			std::string tableName;
+			std::vector<ColumnInfo*> columnInfos;
+		};
+		using DropTableInfo = std::string;
+
+		//===========================================================
+		//DML
 		enum class base_t_t{ LOGICAL_OP, COMPARISON_OP, ID, NUMERIC, STR, MATH_OP };
 		//enum class atom_t_t {  };
 		enum class logical_t_t{ AND, OR };
@@ -52,7 +68,7 @@ namespace DB {
 		struct BaseExpr{
 			BaseExpr(base_t_t base_t):base_t_(base_t){}
 			virtual ~BaseExpr() = 0;
-			virtual RetValue getValue(VM::Row*) = 0;
+			//virtual RetValue getValue(VM::Row*) = 0;
 
 			const base_t_t  base_t_;
 		};
@@ -73,7 +89,7 @@ namespace DB {
 			LogicalOpExpr(logical_t_t logical_t, LogicalOpExpr* left, LogicalOpExpr* right):
 				NonAtomExpr(base_t_t::LOGICAL_OP), logical_t_(logical_t), _left(left), _right(right){}
 			virtual ~LogicalOpExpr();
-			virtual RetValue getValue(VM::Row*);
+			//virtual RetValue getValue(VM::Row*);
 
 			const logical_t_t logical_t_;
 			NonAtomExpr* _left;
@@ -85,7 +101,7 @@ namespace DB {
 			ComparisonOpExpr(comparison_t_t comparison_t, AtomExpr* left, AtomExpr* right):
 				NonAtomExpr(base_t_t::COMPARISON_OP),  comparison_t_(comparison_t), _left(left), _right(right){}
 			virtual ~ComparisonOpExpr();
-			virtual RetValue getValue(VM::Row*);
+			//virtual RetValue getValue(VM::Row*);
 			
 			const comparison_t_t comparison_t_;
 			AtomExpr* _left;
@@ -96,7 +112,7 @@ namespace DB {
 			MathOpExpr(math_t_t math_t, AtomExpr* left, AtomExpr* right):
 				AtomExpr(base_t_t::MATH_OP), math_t_(math_t), _left(left), _right(right){}
 			virtual ~MathOpExpr();
-			virtual RetValue getValue(VM::Row*);
+			//virtual RetValue getValue(VM::Row*);
 
 			const math_t_t math_t_;
 			AtomExpr* _left;
@@ -107,7 +123,7 @@ namespace DB {
 			IdExpr(std::string columnName, std::string tableName = std::string()):
 				AtomExpr(base_t_t::ID), _tableName(tableName), _columnName(columnName){}
 			virtual ~IdExpr();
-			virtual RetValue getValue(VM::Row*);
+			//virtual RetValue getValue(VM::Row*);
 
 			std::string _tableName;
 			std::string _columnName;
@@ -117,7 +133,7 @@ namespace DB {
 			NumericExpr(int value):
 				AtomExpr(base_t_t::NUMERIC), _value(value){}
 			virtual ~NumericExpr();
-			virtual RetValue getValue(VM::Row*);
+			//virtual RetValue getValue(VM::Row*);
 
 			int _value;
 		};
@@ -126,7 +142,7 @@ namespace DB {
 			StrExpr(std::string& value):
 				AtomExpr(base_t_t::STR), _value(value){}
 			virtual ~StrExpr();
-			virtual RetValue getValue();
+			//virtual RetValue getValue();
 
 			std::string _value;
 		};
@@ -138,30 +154,38 @@ namespace DB {
 			bool isDefault;
 			AtomExpr* valueExpr;
 		};
-		class InsertInfo{
+		using Elements = std::vector<Element>;
+		using Source = std::vector<std::string>;
+		struct InsertInfo{
 			std::string sourceTable;
-			// one insert opreation only insert a single row
-			std::vector<Element> elements;
+			std::vector<Elements> values;
 		};
 
-		class UpdateInfo{
+		struct UpdateInfo{
 			std::string sourceTable;
-			std::vector<Element> elements;
+			Elements elements;
 			BaseExpr* whereExpr;
 		};
 
-		class DeleteInfo{
+		struct DeleteInfo{
 			std::string sourceTable;
 			BaseExpr* whereExpr;
 		};
 
-		class SelectInfo{
+		using OrderbyElement = std::pair<BaseExpr*, bool>;
+		struct SelectInfo{
 			bool join;
-			std::vector<std::string> sourceTables;
+			Source sourceTables;
+			Elements elements;
 			BaseExpr* whereExpr;
-			std::vector<std::tuple<std::string, bool>> orderby;	//tuple<column name, is ASC>
+			std::vector<OrderbyElement> orders;	//tuple<column name, is ASC>
 		};
 
+		//===========================================================
+		//visitors	
+		//need reconstruct for atom/id/nonatom
+		inline int numericOp(int op1, int op2, math_t_t math_t);
+		inline bool comparisonOp(int op1, int op2, comparison_t_t comparison_t);
 
 		//output visitor, output the AST for debug
 		class OutputVisitor {
@@ -170,21 +194,37 @@ namespace DB {
 			~OutputVisitor(){ }
 			void visit(BaseExpr* root);
 		private:
-			size_t indent = 0;
+			void _visit(BaseExpr* root);
+			size_t indent;
 		};
 
+		//row visitor, for computing / filtering values of a row
 		class RowVisitor {
 		public:
-			RowVisitor(VM::Row* row):_row(row){ }
+			RowVisitor(){ }
 			~RowVisitor(){ }
-			RetValue visit(BaseExpr* root);
+			RetValue visit(BaseExpr* root, VM::Row* row);
 		private:
-			static inline int numericOp(int op1, int op2, math_t_t math_t);
-			static inline bool comparisonOp(int op1, int op2, comparison_t_t comparison_t);
+			RetValue _visit(BaseExpr* root);
 			VM::Row* _row;
 		};
 
+		//atom visitor, for computing math expression without row info
+		class AtomVisitor {
+		public:
+			AtomVisitor() { }
+			~AtomVisitor() { }
+			RetValue visit(BaseExpr* root);
+		private:
+			RetValue _visit(BaseExpr* root);
+		};
 
-	} //end namespace DML
+		//===========================================================
+		//type for VM
+		struct ErrorMsg {
+			std::string msg;
+		};
+		using SQLValue = std::variant < CreateTableInfo, DropTableInfo, SelectInfo, UpdateInfo, InsertInfo, DeleteInfo, ErrorMsg>;
+	} //end namespace AST
 	
 } //end namespace DB
--- DB/src/ast.cpp
+++ DB/src/ast.cpp
@@ -4,7 +4,7 @@
 #include "dbexception.h"
 
 namespace DB {
-	namespace DML {
+	namespace AST {
 		BaseExpr::~BaseExpr()
 		{
 			
@@ -25,19 +25,11 @@ namespace DB {
 			delete _left;
 			delete _right;
 		}
-		RetValue LogicalOpExpr::getValue(VM::Row* row)
-		{
-			return true;
-		}
 
 		ComparisonOpExpr::~ComparisonOpExpr()
 		{
 			delete _left;
 			delete _right;
-		}
-		RetValue ComparisonOpExpr::getValue(VM::Row* row)
-		{
-			return true;
 		}
 
 		MathOpExpr::~MathOpExpr()
@@ -45,92 +37,123 @@ namespace DB {
 			delete _left;
 			delete _right;
 		}
-		RetValue MathOpExpr::getValue(VM::Row* row)
-		{
-			return 1;
-		}
 
 		IdExpr::~IdExpr()
 		{
 
-		}
-		RetValue IdExpr::getValue(VM::Row* row)
-		{
-			return 2;
 		}
 
 		NumericExpr::~NumericExpr()
 		{
 
 		}
-		RetValue NumericExpr::getValue(VM::Row* row)
-		{
-			return 3;
-		}
 
 		StrExpr::~StrExpr()
 		{
 
 		}
-		RetValue StrExpr::getValue()
-		{
-			return "";
-		}
 
 		//===========================================================
 		//visitors
+		int numericOp(int op1, int op2, math_t_t math_t)
+		{
+			switch (math_t)
+			{
+			case DB::AST::math_t_t::ADD:
+				return op1 + op2;
+			case DB::AST::math_t_t::SUB:
+				return op1 - op2;
+			case DB::AST::math_t_t::MUL:
+				return op1 * op2;
+			case DB::AST::math_t_t::DIV:
+				return op1 / op2;
+			case DB::AST::math_t_t::MOD:
+				return op1 % op2;
+			default:
+				// no expect to happen
+				throw new DB_Exception("wrong math operation type");
+			}
+		}
+
+		bool comparisonOp(int op1, int op2, comparison_t_t comparison_t)
+		{
+			switch (comparison_t)
+			{
+			case DB::AST::comparison_t_t::EQ:
+				return op1 == op2;
+			case DB::AST::comparison_t_t::NEQ:
+				return op1 != op2;
+			case DB::AST::comparison_t_t::LESS:
+				return op1 < op2;
+			case DB::AST::comparison_t_t::GREATER:
+				return op1 > op2;
+			case DB::AST::comparison_t_t::LEQ:
+				return op1 <= op2;
+			case DB::AST::comparison_t_t::GEQ:
+				return op1 >= op2;
+			default:
+				// no expect to happen
+				throw new DB_Exception("wrong comparison operation type");
+			}
+		}
 
 		//output visitor
 
 		void OutputVisitor::visit(BaseExpr* root)
+		{
+			this->indent = 0;
+			this->_visit(root);
+		}
+
+		void OutputVisitor::_visit(BaseExpr* root)
 		{
 			base_t_t base_t = root->base_t_;
 			std::cout << std::string(indent, '-') << base2str[int(base_t)] << "  ";
 			++indent;
 			switch (base_t)
 			{
-			case DB::DML::base_t_t::LOGICAL_OP:
-				{
-					LogicalOpExpr* logicalPtr = static_cast<LogicalOpExpr*>(root);
-					std::cout << logical2str[int(logicalPtr->logical_t_)] << std::endl;
-					visit(logicalPtr->_left);
-					visit(logicalPtr->_right);
-				}
-				break;
-			case DB::DML::base_t_t::COMPARISON_OP:
-				{
-					ComparisonOpExpr* comparisonPtr = static_cast<ComparisonOpExpr*>(root);
-					std::cout << comparison2str[int(comparisonPtr->comparison_t_)] << std::endl;
-					visit(comparisonPtr->_left);
-					visit(comparisonPtr->_right);
-				}
-				break;
-			case DB::DML::base_t_t::MATH_OP:
-				{
-					MathOpExpr* mathPtr = static_cast<MathOpExpr*>(root);
-					std::cout << math2str[int(mathPtr->math_t_)] << std::endl;
-					visit(mathPtr->_left);
-					visit(mathPtr->_right);
-				}
-				break;
-			case DB::DML::base_t_t::ID:
-				{
-					IdExpr* idPtr = static_cast<IdExpr*>(root);
-					std::cout << idPtr->_tableName << "." << idPtr->_columnName << std::endl;
-				}
-				break;
-			case DB::DML::base_t_t::NUMERIC:
-				{
-					NumericExpr* numericPtr = static_cast<NumericExpr*>(root);
-					std::cout << numericPtr->_value << std::endl;
-				}
-				break;
-			case DB::DML::base_t_t::STR:
-				{
-					StrExpr* strPtr = static_cast<StrExpr*>(root);
-					std::cout << strPtr->_value << std::endl;
-				}
-				break;
+			case DB::AST::base_t_t::LOGICAL_OP:
+			{
+				LogicalOpExpr* logicalPtr = static_cast<LogicalOpExpr*>(root);
+				std::cout << logical2str[int(logicalPtr->logical_t_)] << std::endl;
+				visit(logicalPtr->_left);
+				visit(logicalPtr->_right);
+			}
+			break;
+			case DB::AST::base_t_t::COMPARISON_OP:
+			{
+				ComparisonOpExpr* comparisonPtr = static_cast<ComparisonOpExpr*>(root);
+				std::cout << comparison2str[int(comparisonPtr->comparison_t_)] << std::endl;
+				visit(comparisonPtr->_left);
+				visit(comparisonPtr->_right);
+			}
+			break;
+			case DB::AST::base_t_t::MATH_OP:
+			{
+				MathOpExpr* mathPtr = static_cast<MathOpExpr*>(root);
+				std::cout << math2str[int(mathPtr->math_t_)] << std::endl;
+				visit(mathPtr->_left);
+				visit(mathPtr->_right);
+			}
+			break;
+			case DB::AST::base_t_t::ID:
+			{
+				IdExpr* idPtr = static_cast<IdExpr*>(root);
+				std::cout << idPtr->_tableName << "." << idPtr->_columnName << std::endl;
+			}
+			break;
+			case DB::AST::base_t_t::NUMERIC:
+			{
+				NumericExpr* numericPtr = static_cast<NumericExpr*>(root);
+				std::cout << numericPtr->_value << std::endl;
+			}
+			break;
+			case DB::AST::base_t_t::STR:
+			{
+				StrExpr* strPtr = static_cast<StrExpr*>(root);
+				std::cout << strPtr->_value << std::endl;
+			}
+			break;
 			default:
 				std::cout << "Unexpected base_t" << std::endl;
 				break;
@@ -141,188 +164,226 @@ namespace DB {
 
 		//row visitor
 
-		int RowVisitor::numericOp(int op1, int op2, math_t_t math_t)
+		RetValue RowVisitor::visit(BaseExpr* root, VM::Row* row)
 		{
-			switch (math_t)
-			{
-			case DB::DML::math_t_t::ADD:
-				return op1 + op2;
-			case DB::DML::math_t_t::SUB:
-				return op1 - op2;
-			case DB::DML::math_t_t::MUL:
-				return op1 * op2;
-			case DB::DML::math_t_t::DIV:
-				return op1 / op2;
-			case DB::DML::math_t_t::MOD:
-				return op1 % op2;
-			default:
-				// no expect to happen
-				throw new DB_Exception("wrong math operation type");
-			}
+			this->_row = row;
+			return this->_visit(root);
 		}
 
-		bool RowVisitor::comparisonOp(int op1, int op2, comparison_t_t comparison_t)
-		{
-			switch (comparison_t)
-			{
-			case DB::DML::comparison_t_t::EQ:
-				return op1 == op2;
-			case DB::DML::comparison_t_t::NEQ:
-				return op1 != op2;
-			case DB::DML::comparison_t_t::LESS:
-				return op1 < op2;
-			case DB::DML::comparison_t_t::GREATER:
-				return op1 > op2;
-			case DB::DML::comparison_t_t::LEQ:
-				return op1 <= op2;
-			case DB::DML::comparison_t_t::GEQ:
-				return op1 >= op2;
-			default:
-				// no expect to happen
-				throw new DB_Exception("wrong comparison operation type");
-			}
-		}
-
-		RetValue RowVisitor::visit(BaseExpr* root)
+		RetValue RowVisitor::_visit(BaseExpr* root)
 		{
 			base_t_t base_t = root->base_t_;
 			switch (base_t)
 			{
-			case DB::DML::base_t_t::LOGICAL_OP:
-				{
-					LogicalOpExpr* logicalPtr = static_cast<LogicalOpExpr*>(root);
-					RetValue left = visit(logicalPtr->_left);
-					RetValue right = visit(logicalPtr->_right);
-					std::string op = logical2str[int(logicalPtr->logical_t_)];  //used for exception info
+			case DB::AST::base_t_t::LOGICAL_OP:
+			{
+				LogicalOpExpr* logicalPtr = static_cast<LogicalOpExpr*>(root);
+				RetValue left = _visit(logicalPtr->_left);
+				RetValue right = _visit(logicalPtr->_right);
+				std::string op = logical2str[int(logicalPtr->logical_t_)];  //used for exception info
 
-					//RetValue here must be int / str
-					auto op1 = std::get_if<bool>(&left);
-					auto op2 = std::get_if<bool>(&right);
-					if (op1 && op2)
-					{	//op1 op2 are both bool
-						switch (logicalPtr->logical_t_)
+				//RetValue here must be int / str
+				auto op1 = std::get_if<bool>(&left);
+				auto op2 = std::get_if<bool>(&right);
+				if (op1 && op2)
+				{	//op1 op2 are both bool
+					switch (logicalPtr->logical_t_)
+					{
+					case DB::AST::logical_t_t::AND:
+						return *op1 && *op2;
+					case DB::AST::logical_t_t::OR:
+						return *op1 || *op2;
+					default:
+						// no expect to happen
+						throw new DB_Exception("wrong logical type");
+					}
+				}
+				else
+				{	// op1 or op2 is not bool
+					throw new DB_Exception("operation '" + op + "' only support bool oprands");
+				}
+			}
+			case DB::AST::base_t_t::COMPARISON_OP:
+			{
+				ComparisonOpExpr* comparisonPtr = static_cast<ComparisonOpExpr*>(root);
+				RetValue left = _visit(comparisonPtr->_left);
+				RetValue right = _visit(comparisonPtr->_right);
+				std::string op = comparison2str[int(comparisonPtr->comparison_t_)];
+
+				//RetValue here must be int / str
+				if (auto op1 = std::get_if<std::string>(&left))
+				{	//op1 is str
+					if (auto op2 = std::get_if<std::string>(&right))
+					{	//op2 is also str
+						switch (comparisonPtr->comparison_t_)
 						{
-						case DB::DML::logical_t_t::AND:
-							return *op1 && *op2;
-						case DB::DML::logical_t_t::OR:
-							return *op1 || *op2;
+						case DB::AST::comparison_t_t::EQ:
+							return *op1 == *op2;
+						case DB::AST::comparison_t_t::NEQ:
+							return *op1 != *op2;
 						default:
-							// no expect to happen
-							throw new DB_Exception("wrong logical type");
+							throw new DB_Exception("unsupported operation '" + op + "' on strings");
 						}
 					}
 					else
-					{	// op1 or op2 is not bool
-						throw new DB_Exception("operation '" + op + "' only support bool oprands");
+					{	//op is not str
+						throw new DB_Exception("'" + op + "' on mismatched type");
 					}
 				}
-			case DB::DML::base_t_t::COMPARISON_OP:
-				{
-					ComparisonOpExpr* comparisonPtr = static_cast<ComparisonOpExpr*>(root);
-					RetValue left = visit(comparisonPtr->_left);
-					RetValue right = visit(comparisonPtr->_right);
-					std::string op = comparison2str[int(comparisonPtr->comparison_t_)];
+				else if (auto op1 = std::get_if<int>(&left))
+				{	//op1 is int
+					if (auto op2 = std::get_if<int>(&right))
+					{	//op2 is also int
+						return comparisonOp(*op1, *op2, comparisonPtr->comparison_t_);
+					}
+					else
+					{	//int op str/bool
+						throw new DB_Exception("'" + op + "' on mismatched type");
+					}
+				}
+				else
+				{	//op1 is not str/int
+					throw new DB_Exception("opearion '" + op + "' used on unsupported oprands");
+				}
+			}
+			case DB::AST::base_t_t::MATH_OP:
+			{
+				MathOpExpr* mathPtr = static_cast<MathOpExpr*>(root);
+				RetValue left = _visit(mathPtr->_left);
+				RetValue right = _visit(mathPtr->_right);
+				std::string op = math2str[int(mathPtr->math_t_)];	//used for exception info
 
-					//RetValue here must be int / str
-					if (auto op1 = std::get_if<std::string>(&left))
-					{	//op1 is str
-						if (auto op2 = std::get_if<std::string>(&right))
-						{	//op2 is also str
-							switch (comparisonPtr->comparison_t_)
-							{
-							case DB::DML::comparison_t_t::EQ:
-								return *op1 == *op2;
-							case DB::DML::comparison_t_t::NEQ:
-								return *op1 != *op2;
-							default:
-								throw new DB_Exception("unsupported operation '" + op + "' on strings");
-							}
+				//RetValue here must be int / str
+				if (auto op1 = std::get_if<std::string>(&left))
+				{	//op1 is str
+					if (auto op2 = std::get_if<std::string>(&right))
+					{	//op2 is also str
+						if (mathPtr->math_t_ == math_t_t::ADD)
+						{	//str + str
+							return *op1 + *op2;
 						}
 						else
-						{	//op is not str
-							throw new DB_Exception("'" + op + "' on mismatched type");
-						}
-					}
-					else if (auto op1 = std::get_if<int>(&left))
-					{	//op1 is int
-						if (auto op2 = std::get_if<int>(&right))
-						{	//op2 is also int
-							return comparisonOp(*op1, *op2, comparisonPtr->comparison_t_);
-						}
-						else
-						{	//int op str/bool
-							throw new DB_Exception("'" + op + "' on mismatched type");
+						{	// str [-*/%] str
+							throw new DB_Exception("unsupported operation '" + op + "' on string");
 						}
 					}
 					else
-					{	//op1 is not str/int
-						throw new DB_Exception("opearion '" + op + "' used on unsupported oprands");
+					{	//str op int/bool
+						throw new DB_Exception("'" + op + "' on mismatched type");
 					}
 				}
-			case DB::DML::base_t_t::MATH_OP:
-				{
-					MathOpExpr* mathPtr = static_cast<MathOpExpr*>(root);
-					RetValue left = visit(mathPtr->_left);
-					RetValue right = visit(mathPtr->_right);
-					std::string op = math2str[int(mathPtr->math_t_)];	//used for exception info
-
-					//RetValue here must be int / str
-					if (auto op1 = std::get_if<std::string>(&left))
-					{	//op1 is str
-						if (auto op2 = std::get_if<std::string>(&right))
-						{	//op2 is also str
-							if (mathPtr->math_t_ == math_t_t::ADD)
-							{	//str + str
-								return *op1 + *op2;
-							}
-							else
-							{	// str [-*/%] str
-								throw new DB_Exception("unsupported operation '" + op + "' on string");
-							}
-						}
-						else
-						{	//str op int/bool
-							throw new DB_Exception("'" + op + "' on mismatched type");
-						}
-					}
-					else if(auto op1 = std::get_if<int>(&left))
-					{	//op1 is int
-						if (auto op2 = std::get_if<int>(&right))
-						{	//op2 is also int
-							return numericOp(*op1, *op2, mathPtr->math_t_);
-						}
-						else
-						{	//int op str/bool
-							throw new DB_Exception("'" + op + "' on mismatched type");
-						}
+				else if (auto op1 = std::get_if<int>(&left))
+				{	//op1 is int
+					if (auto op2 = std::get_if<int>(&right))
+					{	//op2 is also int
+						return numericOp(*op1, *op2, mathPtr->math_t_);
 					}
 					else
-					{	//op1 is not str/int
-						throw new DB_Exception("opearion '" + op + "' used on unsupported oprands");
+					{	//int op str/bool
+						throw new DB_Exception("'" + op + "' on mismatched type");
 					}
 				}
-			case DB::DML::base_t_t::ID:
-				{
-					IdExpr* idPtr = static_cast<IdExpr*>(root);
-					//fetch and return value for _tableName._columnName
-					return _row->getValue(_row, idPtr->_tableName, idPtr->_columnName);
+				else
+				{	//op1 is not str/int
+					throw new DB_Exception("opearion '" + op + "' used on unsupported oprands");
 				}
-			case DB::DML::base_t_t::NUMERIC:
-				{
-					NumericExpr* numericPtr = static_cast<NumericExpr*>(root);
-					return numericPtr->_value;
-				}
-			case DB::DML::base_t_t::STR:
-				{
-					StrExpr* strPtr = static_cast<StrExpr*>(root);
-					return strPtr->_value;
-				}
+			}
+			case DB::AST::base_t_t::ID:
+			{
+				IdExpr* idPtr = static_cast<IdExpr*>(root);
+				//fetch and return value for _tableName._columnName
+				return VM::getValue(_row, idPtr->_tableName, idPtr->_columnName);
+			}
+			case DB::AST::base_t_t::NUMERIC:
+			{
+				NumericExpr* numericPtr = static_cast<NumericExpr*>(root);
+				return numericPtr->_value;
+			}
+			case DB::AST::base_t_t::STR:
+			{
+				StrExpr* strPtr = static_cast<StrExpr*>(root);
+				return strPtr->_value;
+			}
 			default:
 				// no expect to happen
 				throw new DB_Exception("wrong base type");
 			}
 		}
 
-	} //end namespace DML
+
+		//atom visitor
+		RetValue AtomVisitor::visit(BaseExpr* root)
+		{
+			return this->_visit(root);
+		}
+
+		RetValue AtomVisitor::_visit(BaseExpr* root)
+		{
+			base_t_t base_t = root->base_t_;
+			switch (base_t)
+			{
+			case DB::AST::base_t_t::MATH_OP:
+			{
+				MathOpExpr* mathPtr = static_cast<MathOpExpr*>(root);
+				RetValue left = _visit(mathPtr->_left);
+				RetValue right = _visit(mathPtr->_right);
+				std::string op = math2str[int(mathPtr->math_t_)];	//used for exception info
+
+				//RetValue here must be int / str
+				if (auto op1 = std::get_if<std::string>(&left))
+				{	//op1 is str
+					if (auto op2 = std::get_if<std::string>(&right))
+					{	//op2 is also str
+						if (mathPtr->math_t_ == math_t_t::ADD)
+						{	//str + str
+							return *op1 + *op2;
+						}
+						else
+						{	// str [-*/%] str
+							throw new DB_Exception("unsupported operation '" + op + "' on string");
+						}
+					}
+					else
+					{	//str op int/bool
+						throw new DB_Exception("'" + op + "' on mismatched type");
+					}
+				}
+				else if (auto op1 = std::get_if<int>(&left))
+				{	//op1 is int
+					if (auto op2 = std::get_if<int>(&right))
+					{	//op2 is also int
+						return numericOp(*op1, *op2, mathPtr->math_t_);
+					}
+					else
+					{	//int op str/bool
+						throw new DB_Exception("'" + op + "' on mismatched type");
+					}
+				}
+				else
+				{	//op1 is not str/int
+					throw new DB_Exception("opearion '" + op + "' used on unsupported oprands");
+				}
+			}
+			case DB::AST::base_t_t::NUMERIC:
+			{
+				NumericExpr* numericPtr = static_cast<NumericExpr*>(root);
+				return numericPtr->_value;
+			}
+			case DB::AST::base_t_t::STR:
+			{
+				StrExpr* strPtr = static_cast<StrExpr*>(root);
+				return strPtr->_value;
+			}
+
+			// no expect to happen
+			case DB::AST::base_t_t::LOGICAL_OP:
+			case DB::AST::base_t_t::COMPARISON_OP:
+			case DB::AST::base_t_t::ID:
+			default:
+				throw new DB_Exception("wrong type for atom visitor");
+			}
+		}
+
+	} //end namespace AST
 
 } //end namespace DB
